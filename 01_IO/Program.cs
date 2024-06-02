55db90bfef2fedb128c9cf685c55b8637d805bb6
LotusFoundAnotherWorld <2636258590@qq.com>
2024-06-02 22:12:27 +0800

    initial

---
+++ 01_IO/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+Console.CursorVisible = false;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Clear();
+
+char? inp;
+int initialX = 10;
+int initialY = 10;
+
+while (true)
+{
+    // Console.Clear();
+    Console.SetCursorPosition(initialX, initialY);
+    Console.Write("□");
+
+    inp = Console.ReadKey(true).KeyChar;
+    // 擦除之前位置内容
+    Console.SetCursorPosition(initialX, initialY);
+    Console.Write("  ");
+    switch (inp.ToString()?.ToLower())
+    {
+        case "a":
+            initialX -= 2;
+            if(initialX < 0) initialX = 0;
+            break;
+        case "d":
+            initialX += 2;
+            if(initialX > Console.BufferWidth - 2) initialX = Console.BufferWidth - 2;
+            break;
+        case "w":
+            initialY--;
+            if(initialY < 0) initialY = 0;
+            break;
+        case "s":
+            initialY++;
+            if(initialY > Console.BufferHeight - 1) initialY = Console.BufferHeight - 1;
+            break;
+    }
+}
