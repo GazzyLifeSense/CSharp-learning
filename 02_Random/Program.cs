bb6a5ab0333444e2fdb2872bcc934951e99ff974
lotusFoundAnotherWorld <2636258590@qq.com>
2024-06-03 09:06:49 +0800

    random

---
+++ 02_Random/Program.cs
@@ -0,0 +1,19 @@
+﻿int monsterDef = 10, monsterHp = 20, playerAtk;
+
+while(monsterHp > 0)
+{
+    Console.WriteLine("press any key to attack");
+    Console.ReadKey(true);
+
+    playerAtk = new Random().Next(8, 13);
+    if(playerAtk > monsterDef)
+    {
+        monsterHp -= playerAtk - monsterDef;
+        Console.WriteLine($"atk:{playerAtk}, monsterHp: {monsterHp}");
+    }
+    else
+    {
+        Console.WriteLine($"atk: {playerAtk}, no dmg");
+    }
+}
+Console.WriteLine("you win");
