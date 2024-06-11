6f0a2091c5a61d4839eb78edce3b94d37c9a5082
lotusFoundAnotherWorld <2636258590@qq.com>
2024-06-11 10:20:38 +0800

    operatorReload

---
+++ 05_OperatorReload/Program.cs
@@ -0,0 +1,27 @@
+﻿Vector2 v1 = new Vector2(1, 1);
+Vector2 v2 = new Vector2(2, 1);
+Vector2 v3 = new Vector2(2, 1);
+Console.WriteLine(v1 == v2);
+Console.WriteLine(v3 == v2);
+class Vector2
+{
+    public int x, y;
+
+    public Vector2(int x, int y )
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static bool operator ==( Vector2 a, Vector2 b)
+    {
+        if(a.x == b.x && a.y == b.y) { return true; }
+        return false;
+    }
+
+    public static bool operator !=(Vector2 a, Vector2 b)
+    {
+        if (a.x != b.x || a.y != b.y) { return false; }
+        return true;
+    }
+}
